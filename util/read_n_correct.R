93321d718904b0544eb7a997d9b82f52ccc42f9d
Candace Savonen <cansav09@gmail.com>
2021-02-19 13:54:32 -0500

    Add max_scales and cutoff arguments

--- util/read_n_correct.R
+++ util/read_n_correct.R
@@ -8,7 +8,14 @@
 read_n_correct <- function(filename,
                            wavenum_min,
                            wavenum_max,
-                           return_plots = FALSE) {
+                           peak_threshold = 3,
+                           min_cutoff = NA, 
+                           max_cutoff = NA,
+                           max_scale = NA, 
+                           ridge_length = 32,
+                           window_size = 500,
+                           return_plots = FALSE, 
+                           more_peak_info = TRUE) {
   # Will background correct and identify peaks for a given vibration dataset
   # saved as a CSV file from
   #
@@ -18,12 +25,16 @@ read_n_correct <- function(filename,
   #   wavenum_min/max : two numbers which specify the range of numbers to keep in
   #                    the dataset. Wavenumbers outside these ranges will be
   #                    removed from the data.frame
+  #   min/max_cutoff: If you'd like the data to be filtered before correction then 
+  #                   provide the ranges of wavelengths you'd like to be included.
+  #   ridge_length: passed directly to identifyMajorPeaks() function
   #   return_plots: TRUE/FALSE should the plots be returned?
   #
   # Returns:
   # A data.frame with background corrected intensities and peaks identified.
   # Get file.path
   #
+  
   filename <- file.path(filename)
 
   # Check if the file actually exists
@@ -37,8 +48,40 @@ read_n_correct <- function(filename,
     dplyr::rename("wavenumber" = X1,
                   "intensity" = X2)
   
+  # Filter data based on optional min_cutoff
+  if (!is.na(min_cutoff)){
+  vibration_df <- vibration_df %>% 
+    dplyr::filter(wavenumber > min_cutoff)
+  }
+  
+  # Filter data based on optional max_cutoff
+  if (!is.na(max_cutoff)){
+    vibration_df <- vibration_df %>% 
+      dplyr::filter(wavenumber < max_cutoff)
+  }
+  
+  # If no max_scale is specified, try it out 
+  if (is.na(max_scale)) {
+    # Establish the scale
+    scales <- seq(1, 120, 1)
+  
+    # Try cwt with 120 and see if it fails
+    try_it <- try(cwt(vibration_df$intensity,
+                      # The scales at which to perform CWT
+                      scales = scales,
+                      # Which wavelet base to use
+                      wavelet = 'mexh'), 
+                  silent = TRUE)
+  
+    # Declare max_scale based on the width
+    max_scale <- ifelse(grepl("Error", try_it)[1], 63, 120)
+  }
+  
+  # Print out scale used
+  print(paste("max_scale set to:", max_scale))
+  
   # Establish the scale
-  scales <- seq(1, 120, 1)
+  scales <- seq(1, max_scale, 1)
 
   # Find Continuous Wavelet Transform
   w_coefs <-  cwt(vibration_df$intensity,
@@ -77,8 +120,9 @@ read_n_correct <- function(filename,
 	major_peak_info <- identifyMajorPeaks(vibration_df$intensity,
 	                                      ridge_list,
 	                                      w_coefs,
-	                                      SNR.Th = 0.2,
-	                                      ridgeLength = 1,
+	                                      SNR.Th = peak_threshold,
+	                                      ridgeLength = ridge_length,
+	                                      nearbyWinSize = window_size,
 	                                      nearbyPeak = TRUE)
   # Estimate peak width
 	peak_width <- widthEstimationCWT(vibration_df$intensity,
@@ -105,11 +149,27 @@ read_n_correct <- function(filename,
 	                             f = 0.03)$x,
 	    # Save new column that says which are major peaks
 	    major_peak = (1:nrow(.) %in% major_peak_info$peakIndex),
+	    peak_index = 1:nrow(.),
 	    wavenumber = as.numeric(wavenumber)
 	    ) %>%
 	  dplyr::filter(wavenumber > wavenum_min,
-	                wavenumber < wavenum_max)
-
+	                wavenumber < wavenum_max) 
+	  
+	  # If more_peak_info is TRUE
+	  if (more_peak_info) {
+	    
+	    # Extract SNR based on peak index
+	    peak_snr_df <- data.frame(
+	      peak_index = major_peak_info$peakIndex, 
+	      peak_snr = major_peak_info$peakSNR[match(names(major_peak_info$peakIndex), names(major_peak_info$peakSNR))]
+	      )
+	    
+	    # Join it on
+	    vibration_df <- vibration_df %>% 
+	     dplyr::left_join(peak_snr_df, by = "peak_index")
+	  }
+	
+	
 	# Return the corrected and filtered dataset
 	return(vibration_df)
 }
